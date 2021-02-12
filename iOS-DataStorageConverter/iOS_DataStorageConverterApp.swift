912b7dfa32aeb9f31885f9ea84be225efc453246
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-12 11:10:50 -0500

    Added iOS target using SwiftUI framework to project

---
+++ iOS-DataStorageConverter/iOS_DataStorageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DataStorageConverterApp.swift
+//  iOS-DataStorageConverter
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DataStorageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
